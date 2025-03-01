56be22eed0fe3cdcfe6405aab12f41c4357db261
Kishor Devkota <mekishor61@gmail.com>
2025-03-01 12:21:07 +0000

    add variables lesson 2

---
+++ Lesson2 - Variables/Variables.py
@@ -0,0 +1,27 @@
+#Variables are containers for storing data values. (string, integer,float, boolean)
+#Python has no command for declaring a variable.
+#A variable is created the moment you first assign a value to it.
+#Variables do not need to be declared with any particular type, and can even change type after they have been set.
+
+#string
+first_name = "John"
+last_name ="Doe"
+
+#integer
+age = 20
+
+#float
+salary = 3045.56
+
+#bool
+married = False
+
+print(f"Your name is {first_name} {last_name}")
+print(f"Your age is {age} and your salary is {salary}")
+print(f"Your mariatal status is {married}")
+
+
+x=5
+print(x) # will print 5
+x="apple"
+print(x) # now it will print apple
